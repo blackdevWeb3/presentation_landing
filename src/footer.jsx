9bc68910cd2de5cc45468a59aa6e1647adbb71ef
sm4rtdev <elonscottdev@gmail.com>
2025-02-28 23:08:24 -0500

    fix

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,17 +1,21 @@
-import telegram from "../public/assets/telegram.png";
-import phone from "../public/assets/Phone.png"
-import logo from "../public/assets/mark.png"
-
 const Footer = () => {
   return (
-    <div className=" relative py-[85px] px-[50px] w-full flex max-lg:flex-col max-lg:gap-5 border-t-2 border-t-gray-600 justify-between items-center">
-      <img src={logo} className="absolute"></img>
-      <p className="font-[400] text-[26px] leading-[45px] flex-grow pr-10 text-center">
+    <div className="pl-[50px] pr-[157px] w-full flex max-lg:flex-col max-lg:gap-5 border-t-2 border-t-gray-600 justify-between items-center">
+      <img src="assets/mark.png" alt="mark"></img>
+      <p className="font-[400] text-[26px] leading-[45px] flex-grow text-center">
         For more details or to get started, reach out to us
       </p>
       <div className="flex flex-row gap-4 justify-center items-start">
-        <img src={telegram} alt="telegram"></img>
-        <img src={phone} alt="phone"></img>
+        <img
+          src="assets/Telegram.png"
+          alt="vetor"
+          className="h-[32px] w-auto hover:cursor-pointer"
+        />
+        <img
+          src="assets/Phone.png"
+          alt="vetor"
+          className="h-[32px] w-auto hover:cursor-pointer"
+        />
       </div>
     </div>
   );
